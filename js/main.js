e5bce873049e1059dcc20efaed9a3be66c5d3386
RaminMammadov <50649777+RaminMammadov@users.noreply.github.com>
2020-11-08 01:19:29 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const content = document.getElementById('content');
+const form = document.forms['write'];
+let text = 'auto text writing';
+let i = 0;
+
+
+form.addEventListener('submit',(e) => {
+	e.preventDefault();
+	const val = form['text'].value;
+	text = val;
+})
+
+writeText = () => {
+	content.innerHTML += text[i]
+	i++;
+
+	if(i > text.length) {
+		i = 0;
+		content.innerHTML = '';
+	}
+}
+
+setInterval(writeText, 200);
